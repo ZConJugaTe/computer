f66debe7b4047e6a767515d52cd0b702f4121570
zhangyihang <951850111@qq.com>
2025-01-04 14:38:46 +0800

    Your commit message

---
+++ IF.v
@@ -0,0 +1,71 @@
+`include "lib/defines.vh"
+// 从内存中取指令
+// 使用PC中的地址，从存储器中读取数据，然后将数据放入IF/ID流水线寄存器中。
+// PC地址+4然后写回PC以便为下个时钟周期做好准备，
+// 增加后的地址同时也存入了IF/ID流水线寄存器以备后面的指令使用。
+
+// 新指令一般对其没有影响（除了在添加异常的时候，需要检查指令地址是否出错）
+// P64之前注意跳转指令即可
+
+module IF(
+    input wire clk,
+    input wire rst,
+    input wire [`StallBus-1:0] stall,
+
+    // input wire flush,
+    // input wire [31:0] new_pc,
+
+    input wire [`BR_WD-1:0] br_bus,
+
+    output wire [`IF_TO_ID_WD-1:0] if_to_id_bus,
+
+    output wire inst_sram_en,
+    output wire [3:0] inst_sram_wen,
+    output wire [31:0] inst_sram_addr,
+    output wire [31:0] inst_sram_wdata
+);
+    reg [31:0] pc_reg;
+    reg ce_reg;
+    wire [31:0] next_pc;
+    wire br_e;
+    wire [31:0] br_addr;
+// 转移信息
+    assign {
+        br_e,
+        br_addr
+    } = br_bus;
+
+// 时序逻辑
+    always @ (posedge clk) begin
+        if (rst) begin
+            pc_reg <= 32'hbfbf_fffc;
+        end
+        else if (stall[0]==`NoStop) begin
+            pc_reg <= next_pc;
+        end
+    end
+
+    always @ (posedge clk) begin
+        if (rst) begin
+            ce_reg <= 1'b0;
+        end
+        else if (stall[0]==`NoStop) begin
+            ce_reg <= 1'b1;
+        end
+    end
+
+
+    assign next_pc = br_e ? br_addr 
+                   : pc_reg + 32'h4;
+
+    
+    assign inst_sram_en = ce_reg;
+    assign inst_sram_wen = 4'b0;
+    assign inst_sram_addr = pc_reg;
+    assign inst_sram_wdata = 32'b0;
+    assign if_to_id_bus = {
+        ce_reg,
+        pc_reg
+    };
+
+endmodule
